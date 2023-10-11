ed53fb9eb9041cd27d28b27f7e47b5e8af192add
safakcivelek <safakcivelek3@gmail.com>
2023-10-12 00:37:52 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+// euroDun=11   // Bunu yapamayız!
+console.log(euroDun)
+
+//array
+let konutKredileri =["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+
+for(let i = 0; i < konutKredileri.length ; i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
+
+console.log(konutKredileri)
